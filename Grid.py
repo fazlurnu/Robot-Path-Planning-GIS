52284ae12dee1187185678e85a2a58c0d0c36f5e
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-26 17:11:28 +0800

    change structure

--- Grid.py
+++ Grid.py
@@ -13,18 +13,17 @@ from pygame.locals import *
 
 class Grid:
     
-    def __init__(self, maze, paths):
-        self.cols = len(maze)
-        self.rows = len(maze[0])
+    def __init__(self, coordinates, start, end, edges):
         
-        print("Hello from grid, " + str(self.rows) + "x" + str(self.cols))
-    
-        self.width = 100
-        self.height = 100
+        row = 10
+        col = 10
+
+        self.width = 50
+        self.height = 50
         
         pygame.init()
         pygame.display.set_caption('Python Code for Robot Command')
-        self.screen = pygame.display.set_mode((self.cols * self.width, self.rows * self.height))
+        self.screen = pygame.display.set_mode((row * self.width, col * self.height))
         
         pygame.init()
 
@@ -34,33 +33,51 @@ class Grid:
         BLACK=(0,0,0)
         RED=(255, 0, 0)
         GREEN=(0,255,0)
+        BLUE=(0,0,255)
         self.screen.fill(WHITE)
 
-        border_width = 5
-        
-        for i, row in enumerate(maze):
-            for j, cell in enumerate(row):
-                if (cell):
-                    pygame.draw.rect(self.screen,BLACK,(j*self.width + border_width, i*self.height + border_width, self.width - border_width,self.height - border_width))
-                else:
-                    pygame.draw.rect(self.screen,TOSCA,(j*self.width + border_width, i*self.height + border_width, self.width - border_width,self.height - border_width))
-                    
-        for i in range (1, len(paths)):
+
+        for edge in edges:
+            start_node = edge[0]
+            end_node = edge[1]
+
+            start_point_y = int(start_node[0]*self.width + self.width/2)
+            start_point_x = int(start_node[1]*self.height + self.height/2)
+            
+            end_point_y = int(end_node[0]*self.width + self.width/2)
+            end_point_x = int(end_node[1]*self.height + self.height/2)
+
+            pygame.draw.line(self.screen, BLACK, (start_point_x, start_point_y), 
+                            (end_point_x, end_point_y), 3)
+
+        for coordinate in coordinates:
+           point_y = int(coordinate[0]*self.width + self.width/2)
+           point_x = int(coordinate[1]*self.height + self.height/2)
+
+           pygame.draw.circle(self.screen, GREEN, [point_x, point_y], 5) 
+
+        start_point_y = int(start[0]*self.width + self.width/2)
+        start_point_x = int(start[1]*self.height + self.height/2)
+
+        pygame.draw.circle(self.screen, BLUE, [start_point_x, start_point_y], 5)
+
+        end_point_y = int(end[0]*self.width + self.width/2)
+        end_point_x = int(end[1]*self.height + self.height/2)
+
+        pygame.draw.circle(self.screen, RED, [end_point_x, end_point_y], 5)
+
+        """for i in range (1, len(paths)):
            start_point_y = int(paths[i-1][0]*self.width + self.width/2)
            start_point_x = int(paths[i-1][1]*self.height + self.height/2)
             
            end_point_y = int(paths[i][0]*self.width + self.width/2)
            end_point_x = int(paths[i][1]*self.height + self.height/2)
            
-           pygame.draw.line(self.screen, RED, (start_point_x, start_point_y), 
-                            (end_point_x, end_point_y), 10)
-           
-           if (i==1):
-               pygame.draw.circle(self.screen, GREEN, [start_point_x, start_point_y], 20)
-               
-           if (i==len(paths)-1):
-               pygame.draw.circle(self.screen, RED, [end_point_x, end_point_y], 20)
-                       
+           #pygame.draw.line(self.screen, RED, (start_point_x, start_point_y), 
+                            #(end_point_x, end_point_y), 10)
+            
+        pygame.draw.circle(self.screen, GREEN, [start_point_x, start_point_y], 5) 
+        """
         
         while True:
             for event in pygame.event.get():
--- main.py
+++ main.py
@@ -4,29 +4,45 @@ Created on Thu Jun 25 21:37:10 2020
 
 @author: My Laptop
 """
-from astar import astar
+#from astar import astar
 from Grid import Grid
+import random
 
 def main():
 
-    maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    nodes = []
 
-    start = (0, 0)
-    end = (8, 9)
+    for i in range(10):
+        for j in range(10):
+            
+            node = (i,j)
+            nodes.append(node)
 
-    path = astar(maze, start, end)
+    #print(coordinates)
+
+    start = nodes[0]
+    end = nodes[len(nodes)-1]
+
     
-    print(path)
-    grid = Grid(maze, path)
+    edges = []
+
+    for i in range(10):
+        for j in range(9):
+            edge = [nodes[i*10+j], nodes[i*10+j+1]]
+            edges.append(edge)
+
+    for i in range(1, 10):
+        for j in range(10):
+            index = i*10+j
+            edge = [nodes[index], nodes[index-10]]
+            edges.append(edge)
+
+    print(edges)
+    
+    #path = astar(maze, start, end)
+    
+    #print(path)
+    grid = Grid(nodes, start, end, edges)
 
 
 if __name__ == '__main__':
