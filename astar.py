d5a8ee5f47c6a3df0de9c6cbcc169aedccf5caa1
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-28 06:34:42 +0800

    add astar

---
+++ astar.py
@@ -0,0 +1,70 @@
+from Node import Node
+
+# A* search
+def astar_search(graph, heuristics, start, end):
+    
+    # Create lists for open nodes and closed nodes
+    open = []
+    closed = []
+
+    # Create a start node and an goal node
+    start_node = Node(start, None)
+    goal_node = Node(end, None)
+
+    # Add the start node
+    open.append(start_node)
+    
+    # Loop until the open list is empty
+    while len(open) > 0:
+
+        # Sort the open list to get the node with the lowest cost first
+        open.sort()
+
+        # Get the node with the lowest cost
+        current_node = open.pop(0)
+
+        # Add the current node to the closed list
+        closed.append(current_node)
+        
+        # Check if we have reached the goal, return the path
+        if current_node == goal_node:
+            path = []
+            while current_node != start_node:
+                path.append(current_node.name + ': ' + str(current_node.g))
+                current_node = current_node.parent
+            path.append(start_node.name + ': ' + str(start_node.g))
+            # Return reversed path
+            return path[::-1]
+
+        # Get neighbours
+        neighbors = graph.get(current_node.name)
+
+        # Loop neighbors
+        for key, value in neighbors.items():
+
+            # Create a neighbor node
+            neighbor = Node(key, current_node)
+
+            # Check if the neighbor is in the closed list
+            if(neighbor in closed):
+                continue
+
+            # Calculate full path cost
+            neighbor.g = current_node.g + graph.get(current_node.name, neighbor.name)
+            neighbor.h = heuristics.get(neighbor.name)
+            neighbor.f = neighbor.g + neighbor.h
+
+            # Check if neighbor is in open list and if it has a lower f value
+            if(add_to_open(open, neighbor) == True):
+                # Everything is green, add neighbor to open list
+                open.append(neighbor)
+
+    # Return None, no path is found
+    return None
+
+# Check if a neighbor should be added to open list
+def add_to_open(open, neighbor):
+    for node in open:
+        if (neighbor == node and neighbor.f > node.f):
+            return False
+    return True
